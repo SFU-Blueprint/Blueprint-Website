e8cf3d80532cae722ced4d11057164864b633773
ronneylogs <ronneylok@gmail.com>
2024-01-04 01:45:11 -0800

    added custom blueprint font and colours, finished top section of about page with responsiveness taken care of

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,27 +3,47 @@ import React from "react";
 const AboutPage = () => {
   return (
     <div>
-      <div className="border-black border-2 p-40 text-center text-3xl font-bold underline">About Page Image</div>
-      <div className="m-32">
-        <h1 className="text-5xl font-bold ">Building innovative, tech-based solutions for communities and public welfare is the mission that brings us together. </h1>
-        <div className="m-20 grid grid-cols-2">
-          <div>
-            <p>Our talented members come from diverse cultures, professions, and social backgrounds.
-             With a passion for social good and dedication to creating beautiful technology, our project teams work alongside nonprofits to help them better serve their communities.</p>
-             <h1 className="text-xl text-blueprint-blue font-bold mt-10 mb-7"> SFU BLUEPRINT</h1>
-             <p>Blueprint is a multinational community with chapters run by students from different universities. As the third established chapter in Canada, our team is based at <span>
-                <a href="https://www.sfu.ca"className="underline">Simon Fraser University.</a>
-              </span>
-            </p>
-            <p className="underline font-bold mt-6">See all Blueprint chapters here</p>
-          </div>
-          <div>
-            <img className="float-right" src="" alt="" />
-            <img className="float-left" src="" alt="" />
+        <div className="relative">
+          <img src="/testImage.jpg" alt="Your" class="w-full h-auto"/>
+          <div className="absolute left-0 bottom-5 mx-5 md:mx-10 md:my-10 justify-center">
+            <p className="font-anek text-black text-4xl my-1.5 font-bold md:text-5xl ">About Us</p>
+            <p className="font-poppins font-bold md:text-2xl" >TECH-DRIVEN ADVOCATED FOR SOCIAL GOOD</p>
           </div>
         </div>
-        
+      <div className="m-6 md:m-10">
+        <h1 className="font-anek font-bold text-2xl md:text-5xl">Building innovative, tech-based solutions for communities and public welfare is the mission that brings us together. </h1>
+        <div className="ml-20 mt-5 md:m-10 md:grid md:grid-cols-2">
+          <div>
+            <p className="font-poppins">Our talented members come from diverse cultures, professions, and social backgrounds.
+             With a passion for social good and dedication to creating beautiful technology, our project teams work alongside nonprofits to help them better serve their communities.</p>
+             <h1 className="font-poppins text-xl text-blueprint-blue font-bold mt-10 mb-7"> SFU BLUEPRINT</h1>
+             <p>Blueprint is a multinational community with chapters run by students from different universities. As the third established chapter in Canada, our team is based at <span>
+                <a href="https://www.sfu.ca"className="underline font-poppins">Simon Fraser University.</a>
+              </span>
+            </p>
+            <p className="font-poppins underline font-bold mt-6">See all Blueprint chapters here</p>
+          </div>
+          <div className="hidden md:block md:space-y-20 flex">
+            <div className="flex">
+              <img className="ml-20 size-4/5" src="/testImage.jpg" alt="" />
+            </div>
+            <div className="flex">
+              <img className="bottom-0 size-3/5 " src="/testImage.jpg" alt="" />
+              <p className="flex-1 font-sketch pl-10">A design sprint is the best way to get ideas flowing</p>
+            </div>
+          </div>
+        </div>
       </div>
+      <div className="block md:hidden">
+        <img className="" src="/testImage.jpg" alt="" />
+        <div className="m-5 mt-5 grid grid-cols-2">
+          <img src="/testImage.jpg" alt="" />
+          <p className="flex-1 font-sketch pl-10">A design sprint is the best way to get ideas flowing</p>
+        </div>
+      </div>
+
+      
+
       
 
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,15 @@ module.exports = {
     extend: {
       colors: {
         'blueprint-blue': '#0078e8',
+        'blueprint-secondaryBlue': '#E0F0FF',
+        'blueprint-sky': '#93C9FE',
+        'blueprint-black': '#CCCCCC'
       },
-
-
+      fontFamily:{
+        poppins:["Poppins","sans-serif"],
+        anek:["Anek Bangla","sans-serif"],
+        sketch:["Just Me Again Down Here", "sans-serif"]
+      }
     },
   },
   plugins: [],
