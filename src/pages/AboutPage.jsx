be4cdd3ca5fa293461b4ed155f82067126781d14
CWRalph <colwynralph15@gmail.com>
2024-01-14 18:32:22 -0800

    Fixed formatting issue when screen is small in the about us page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import GearSection from "../components/Gears/GearSection";
-import Gear from "../components/Gears/Gear";
 import MobileGearSection from "../components/Gears/MobileGearSection";
 
+/**
+ * React functional component representing the About Us page.
+ *
+ * @component
+ */
 const AboutPage = () => {
+  //State used to hold the current gear section JSX (can either be the mobile or desktop version)
+  const [gearSectionHtml, setGearSectionHtml] = useState({
+    content: <GearSection></GearSection>
+  });
+
+  useEffect(() => {
+    const resize = () => {
+      const windowWidth = window.innerWidth;
+      const GEAR_SECTION_WIDTH_BREAKPOINT = 1000;
+
+      // Update the gear section between mobile/desktop based on the screen width.
+      setGearSectionHtml({
+        content: windowWidth > GEAR_SECTION_WIDTH_BREAKPOINT
+            ? <GearSection></GearSection>
+            : <MobileGearSection></MobileGearSection>
+      })
+    }
+
+    // Add the event listener on component mount and remove on unmount.
+    window.addEventListener('resize', resize)
+
+    return () => {
+      window.removeEventListener('resize', resize);
+    };
+  }, []);
+
+  /**
+   * JSX for rendering the About page.
+   *
+   * @returns {JSX.Element} The About page component.
+   */
+
   return (
     <div>
       {/* About us */}
@@ -26,7 +62,7 @@ const AboutPage = () => {
             <div className="hidden md:block w-96 h-56 bg-zinc-300 rounded-sm flex-4"></div>
         </div>  */}
         <div className="flex flex-col ml-[20%] md:w-4/5 md:ml-[11%] md:flex-row mt-[24px] md:mt-[64px] md:justify-between ">
-          <p className="md:w-2/5 text-xs font-poppins tracking-tight md:tracking-wide md:text-base md:font-normal">
+          <p className="md:w-2/5 text-xs font-poppins tracking-tight md:tracking-wide md:text-base md:font-normal h-fit">
             Our talented members come from diverse cultures, professions, and
             social backgrounds. With a passion for social good and dedication to
             creating beautiful technology, our project teams work alongside
@@ -38,13 +74,13 @@ const AboutPage = () => {
         <div className="ml-[20%] flex flex-col mt-[24px] md:ml-[11%] md:w-4/5 md:justify-start">
           <h1 className="font-poppins text-base font-medium text-xl text-blueprint-blue mt-5 md:mt-10 mb-2 md:text-2xl"> SFU BLUEPRINT</h1>
           <div className="flex flex-row md:justify-between md:items-start md:gap-36 md:inline-flex">
-            <p className="font-poppins w-72 text-xs tracking-tight md:tracking-wide md:w-96 md:h-36 md:text-base md:font-normal">Blueprint is a multinational community with chapters run by students from different universities. As the third established chapter in Canada, our team is based at <span>
+            <p className="font-poppins w-72 text-xs tracking-tight md:tracking-wide md:w-96 md:h-36 md:text-base md:font-normal h-fit">Blueprint is a multinational community with chapters run by students from different universities. As the third established chapter in Canada, our team is based at <span>
                 <a href="https://www.sfu.ca"className="underline font-poppins w-72 text-xs tracking-tight md:tracking-wide md:w-96 md:h-36 md:text-base md:font-normal">Simon Fraser University.</a>
               </span>
             </p>
             <div className="md:justify-start md:items-start inline-flex">
               <div className="hidden md:block w-80 h-40 bg-zinc-300 rounded-sm"></div>
-              <p className="pl-10 hidden md:block font-sketch text-xl w-36 h-20 leading-none tracking-tight">A design sprint is the best way to get ideas flowing</p>
+              <p className="pl-10 hidden md:block font-sketch text-xl w-36 h-20 leading-none tracking-tight h-fit">A design sprint is the best way to get ideas flowing</p>
             </div>
           </div>
           <p className="font-poppins underline text-xs font-bold mt-4 md:mt-6 md:text-base">See all Blueprint chapters here</p>
@@ -54,10 +90,11 @@ const AboutPage = () => {
         <div className="block md:hidden w-screen h-60 bg-zinc-300 justify-self-center rounded-sm "></div>
         <div className="m-[10%] flex flex-row">
           <div className="block basis-4/6 md:hidden w-64 h-32 bg-zinc-300 rounded-sm "></div>
-          <p className="basis-2/6 font-sketch pl-[10%]">A design sprint is the best way to get ideas flowing</p>
+          <p className="basis-2/6 font-sketch pl-[10%] h-fit">A design sprint is the best way to get ideas flowing</p>
         </div>
       </div>
-      <GearSection></GearSection>
+      <div className="mx-6 md:mx-20 font-anek text-black text-4xl md:text-5xl font-[550]">Our Values</div>
+      {/*{gearSectionHtml.content}*/}
     </div>
   );
 };
