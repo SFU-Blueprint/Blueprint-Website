11dc4eec8b21ae8b664cde3d55f4278147d5b6a8
CWRalph <colwynralph15@gmail.com>
2024-01-25 19:20:54 -0800

    Made home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Footer from "../components/Footer"
+import OutlineButton from "../components/Shared/OutlineButton"
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <OutlineButton></OutlineButton>
     </div>
   );
 };
