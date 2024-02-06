8d8bcab34e04df61ba8c314e9e8f93a98a54df5d
CWRalph <colwynralph15@gmail.com>
2024-02-06 00:27:11 -0800

    Implemented translation library, tested it on home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {useTranslation} from "react-i18next";
+import {PageHeader} from "../components/Common";
 
 const HomePage = () => {
   const {t, i18n} = useTranslation()
   return (
     <div>
-      <h1>{t('home.title')}</h1>
+      <PageHeader>{t('home.title')}</PageHeader>
     </div>
   );
 };
