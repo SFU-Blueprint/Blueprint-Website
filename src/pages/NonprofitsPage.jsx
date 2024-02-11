4149f672cc5e801bba9ae5aad4d7fffff6da8ca7
Terry <terrykwok147@gmail.com>
2024-02-11 14:59:43 -0800

    Implementating translation to NonprofitsPage.jsx

--- src/pages/NonprofitsPage.jsx
+++ src/pages/NonprofitsPage.jsx
@@ -1,68 +1,67 @@
 import React, { useState } from "react";
-import { BodyHeader, PageHeader, SectionHeader, Body } from "../components/Common";
+import {
+  BodyHeader,
+  PageHeader,
+  SectionHeader,
+  Body,
+} from "../components/Common";
+import { useTranslation } from "react-i18next";
 
 const NonprofitsPage = () => {
+  const { t, i18n } = useTranslation();
   const [activePhase, setActivePhase] = useState(0);
-  const phases = [
-    "Initial Consulting",
-    "Product Research and Scoping",
-    "Building the Minimum Viable Product",
-    "User Testing",
-    "Product Polish and Hands Off",
-  ];
-  const content = (title) => ({
-    title: title,
-    text: {
-      "Organizational Need":
-        "How does this product improve current business processes (% efficiency, $ saved etc.)? How does it align with long-term organizational vision? How urgently does this product need to be built to address existing concerns?",
-      "Community Impact":
-        "How does this product impact the people that the non-profit serves? Does the non-profit understand the challenges encountered by the community in need and how this solution aligns with solving these problems?",
-      "Technical Feasibility":
-        "Are the desired features commonly built-in software products or near the frontiers of what can be built? Some examples of solutions we can achieve include: Mobile apps, Websites, In-browser games, Information databases, & AI/machine learning solutions.",
-      "Project Scope":
-        "Can the product be realistically completed in a 4/8/12 month timeline? Are there any external factors such as user interviews and third-party services that could prolong the project?",
-      "Initial Consulting":
-        "After initial outreach, our executive team will meet with the NPO to learn about their request. After the first meeting, our VP scoping will consult members with expertise to understand how Blueprint can take on the project and get back to the NPO with description of possible projects.\n\n Once we have agreed on the high-level direction of the project, Blueprint will assemble the project team and start the project in the next semester. ",
-      "Product Research and Scoping":
-        "To kickstart the collaboration process, our teams will work closely with the nonprofit to gain a greater understanding of project needs and priorities as well as scope out the minimum viable product (MVP).\n\n From this point, the product manager will curate a statement of work (SOW) that defines project scope capturing the discussion with the nonprofit.",
-      "Building the Minimum Viable Product":
-        "This third phase is where the magic begins! Our teams work hard to get the MVP up and running. Throughout this phase, we will stay in touch with the nonprofit to ensure smooth development and consistent alignment.",
-      "User Testing":
-        "Once the MVP is built and released to an initial group of end users, we conduct rigorous testing to ensure no critical problems remain. Additionally, we will take note of any bugs, UX issues, and feature requests during this time.",
-      "Product Polish and Hands Off":
-        "This phase involves intensive development and design work. Our team will execute improvements on the product based on feedback received from the user testing phase.\n\n The objective of this final phase is to ensure the quality and completeness of the finished product. Upon product completion, our team will meet with the nonprofit to walk through maintenance best practices before hand off.",
-    }[title],
-  });
+  const phases = t("nonProfits.phases");
+  // const content = (title) => ({
+  //   title: title,
+  //   text: {
+  //     "Organizational Need":
+  //       "How does this product improve current business processes (% efficiency, $ saved etc.)? How does it align with long-term organizational vision? How urgently does this product need to be built to address existing concerns?",
+  //     "Community Impact":
+  //       "How does this product impact the people that the non-profit serves? Does the non-profit understand the challenges encountered by the community in need and how this solution aligns with solving these problems?",
+  //     "Technical Feasibility":
+  //       "Are the desired features commonly built-in software products or near the frontiers of what can be built? Some examples of solutions we can achieve include: Mobile apps, Websites, In-browser games, Information databases, & AI/machine learning solutions.",
+  //     "Project Scope":
+  //       "Can the product be realistically completed in a 4/8/12 month timeline? Are there any external factors such as user interviews and third-party services that could prolong the project?",
+  //     "Initial Consulting":
+  //       "After initial outreach, our executive team will meet with the NPO to learn about their request. After the first meeting, our VP scoping will consult members with expertise to understand how Blueprint can take on the project and get back to the NPO with description of possible projects.\n\n Once we have agreed on the high-level direction of the project, Blueprint will assemble the project team and start the project in the next semester. ",
+  //     "Product Research and Scoping":
+  //       "To kickstart the collaboration process, our teams will work closely with the nonprofit to gain a greater understanding of project needs and priorities as well as scope out the minimum viable product (MVP).\n\n From this point, the product manager will curate a statement of work (SOW) that defines project scope capturing the discussion with the nonprofit.",
+  //     "Building the Minimum Viable Product":
+  //       "This third phase is where the magic begins! Our teams work hard to get the MVP up and running. Throughout this phase, we will stay in touch with the nonprofit to ensure smooth development and consistent alignment.",
+  //     "User Testing":
+  //       "Once the MVP is built and released to an initial group of end users, we conduct rigorous testing to ensure no critical problems remain. Additionally, we will take note of any bugs, UX issues, and feature requests during this time.",
+  //     "Product Polish and Hands Off":
+  //       "This phase involves intensive development and design work. Our team will execute improvements on the product based on feedback received from the user testing phase.\n\n The objective of this final phase is to ensure the quality and completeness of the finished product. Upon product completion, our team will meet with the nonprofit to walk through maintenance best practices before hand off.",
+  //   }[title],
+  // });
 
   return (
     <div className="flex flex-col">
       <div className="w-full h-[500px] px-[5%] md:px-[15%] pt-[200px] bg-blueprint-gray-light">
-        <PageHeader>For Non-Profits</PageHeader>
+        <PageHeader>{t("nonProfits.header")}</PageHeader>
         <BodyHeader className="mt-2 mb-4">
-          We can turn your technological vision into reality - for free
+          {t("nonProfits.forNonProfits")}
         </BodyHeader>
         <ProposeButton />
       </div>
       <div className="w-full px-[5%] md:px-[15%]">
-        <SectionHeader className="mt-8 mb-5">Why collaborate with us?</SectionHeader>
+        <SectionHeader className="mt-8 mb-5">
+          Why collaborate with us?
+        </SectionHeader>
         <Body className="w-full md:w-[720px]">
-          Our members are highly dedicated and have great expertise in
-          technology and design. By working with us, your organization will gain
-          fresh perspectives of your business, increase community engagement
-          with local students, and bring your vision for social good to live
-          through innovative ways, free of charge.
+          {t("nonProfits.whyCollaborateWithUs")}
         </Body>
-        <SectionHeader className="mt-8 mb-5">Can your idea become a project?</SectionHeader>
-        <Body>
-          We consider the following aspects when evaluating potential projects:
-        </Body>
+        <SectionHeader className="mt-8 mb-5">
+          Can your idea become a project?
+        </SectionHeader>
+        <Body>{t("nonProfits.canYourIdeaBecomeAProject")}</Body>
         <div className="flex max-md:flex-col gap-8 mb-8 md:gap-16 md:mb-10 mt-5">
-          <ContentCard>{content("Organizational Need")}</ContentCard>
-          <ContentCard>{content("Technical Feasibility")}</ContentCard>
+          <ContentCard>{"organizationalNeed"}</ContentCard>
+          <ContentCard>{"technicalFeasibility"}</ContentCard>
         </div>
         <div className="flex max-md:flex-col gap-8 mb-8 md:gap-16">
-          <ContentCard>{content("Community Impact")}</ContentCard>
-          <ContentCard>{content("Project Scope")}</ContentCard>
+          <ContentCard>{"communityImpact"}</ContentCard>
+          <ContentCard>{"projectScope"}</ContentCard>
         </div>
         <SectionHeader className="mt-16 mb-5">Our Process</SectionHeader>
         <div className="flex max-md:flex-col gap-16">
@@ -87,7 +86,7 @@ const NonprofitsPage = () => {
                       index === activePhase && "text-blueprint-white"
                     } !tracking-normal`}
                   >
-                    {phase}
+                    {t(`nonProfits.${phase}.name`)}
                   </Body>
                 </button>
                 <ContentCard
@@ -95,26 +94,26 @@ const NonprofitsPage = () => {
                     index !== activePhase && "hidden"
                   } md:hidden w-full py-8`}
                 >
-                  {content(phases[activePhase])}
+                  {phase}
+                </ContentCard>
+                <ContentCard className="max-md:hidden pt-32">
+                  {phase}
                 </ContentCard>
               </div>
             ))}
           </div>
-          <ContentCard className="max-md:hidden pt-32">
-            {content(phases[activePhase])}
-          </ContentCard>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center w-full h-[500px] bg-gray-200 mt-16  ">
-        <SectionHeader className="my-5 text-center">Send your ideas to us now</SectionHeader>
+        <SectionHeader className="my-5 text-center">
+          Send your ideas to us now
+        </SectionHeader>
         <Body className="!tracking-wide text-center whitespace-pre-line">
-          {
-            "Interested in working with us? Submit your initial project proposal below!\n We will get back to you shortly."
-          }
+          {t("nonProfits.sendYourIdeasToUsNow")}
         </Body>
         <ProposeButton className="my-5 border-blueprint-blue text-blueprint-blue" />
         <Body className="my-10 !tracking-wide text-center">
-          Have a question? Feel free to shoot us an email at{" "}
+          {t("nonProfits.callToContact")}{" "}
           <a
             href="mailto:sfublueprint@gmail.com"
             className="text-blueprint-blue hover:underline hover:font-bold"
@@ -128,14 +127,15 @@ const NonprofitsPage = () => {
   );
 };
 
-
-
 function ContentCard({ children, className }) {
-  const { title, text } = children;
+  const { t, i18n } = useTranslation();
+  const title = children;
   return (
     <div className={`${className} flex flex-col w-full md:w-[420px] gap-1`}>
-      <BodyHeader>{title}</BodyHeader>
-      <Body>{text}</Body>
+      <BodyHeader className="text-blueprint-blue">
+        {t(`nonProfits.${title}.name`)}
+      </BodyHeader>
+      <Body>{t(`nonProfits.${title}.description`)}</Body>
     </div>
   );
 }
