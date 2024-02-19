ae321bb0d49717155c07aa5674488c902a8619ec
ronneylogs <ronneylok@gmail.com>
2024-02-18 21:50:35 -0800

    added responsiveness and images

---
+++ src/constants/Event.js
@@ -0,0 +1,41 @@
+export const Event1 = [
+  {
+    title: "Halloween",
+    img: "/events/halloween.jpeg",
+    date: "October 29th 2023",
+  },
+  {
+    title: "Info",
+    img: "/events/info.jpg",
+    date: "January 19th 2024",
+  },
+  {
+    title: "Tech",
+    img: "/events/tech.jpg",
+    date: "November 23rd 2023",
+  },
+  {
+    title: "Club",
+    img: "/events/club.jpeg",
+    date: "September 17th 2023",
+  },
+];
+
+export const Event2 = [
+  {
+    title: "Halloween",
+    img: "/events/halloween.jpeg",
+    date: "October 29th 2023",
+  },
+  {
+    title: "Info",
+    img: "/events/info.jpg",
+    date: "January 19th 2024",
+  },
+
+  {
+    title: "Club",
+    img: "/events/club.jpeg",
+    date: "September 17th 2023",
+  },
+];
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { ParagraphTitle, Landing, PageHeader, SectionHeader, ParagraphText } from "../components/Common";
+import {
+  ParagraphTitle,
+  Landing,
+  PageHeader,
+  SectionHeader,
+  ParagraphText,
+} from "../components/Common";
 import ProjectCard from "../components/projects-page/ProjectCard";
+import { Event1, Event2 } from "../constants/Event";
 
 const HomePage = () => {
-  const { t, i18n } = useTranslation()
+  const { t, i18n } = useTranslation();
 
   //Favorite projects for home page
   const projects = ["smartAssistant", "aiForHealth", "dataTracking"];
@@ -23,8 +30,8 @@ const HomePage = () => {
   };
 
   return (
-    <div className="flex flex-col px-3 md:px-[15%] pt-10 gap-4">
-      <div className="mb-[100px] mx-12">
+    <div className="flex flex-col pt-10 gap-4">
+      <div className="mb-[100px] mx-12 px-3 md:px-[15%] ">
         <Landing className="!font-semibold md:w-[600px]">
           {t("home.header.title")}
         </Landing>
@@ -35,7 +42,7 @@ const HomePage = () => {
           {t("home.header.button")}
         </button>
       </div>
-      <div className="mt-60">
+      <div className="mt-60 px-3 md:px-[15%] ">
         <SectionHeader>{t("home.aboutUs.title")}</SectionHeader>
         <ParagraphText className="md:w-[500px] mb-3 md:mb-6">
           {t("home.aboutUs.text")}
@@ -45,13 +52,15 @@ const HomePage = () => {
         </button>
       </div>
 
-
-      <div className="px-16 py-12 mt-16 w-full max-md:px-5 max-md:mt-10 bg-zinc-100">
-        <SectionHeader className="mb-10">Get Involved</SectionHeader>
-        <div className="flex max-md:flex-col justify-between gap-9 mb-16 ">
+      <div className="px-16 py-12 mt-16 w-full max-md:px-5 max-md:mt-10 bg-zinc-100 px-3 md:px-[15%] ">
+        <SectionHeader className="mb-10  ">Get Involved</SectionHeader>
+        <div className="flex max-md:flex-col justify-between gap-12 mb-16 ">
           <div className="flex flex-col justify-start h-48 gap-2">
             <ParagraphTitle>STUDENTS</ParagraphTitle>
-            <ParagraphText>Contribute to building technology for social good. Make new friends and harness your professional skillsets along the away!</ParagraphText>
+            <ParagraphText>
+              Contribute to building technology for social good. Make new
+              friends and harness your professional skillsets along the away!
+            </ParagraphText>
             <button className="text-blueprint-blue border-blueprint-blue flex justify-center items-center w-[200px] p-4 rounded-md border-2 font-poppins font-bold tracking-tight hover:shadow-2xl">
               Join us
             </button>
@@ -59,9 +68,11 @@ const HomePage = () => {
 
           <div className="flex flex-col justify-start h-48 gap-2">
             <ParagraphTitle>NON-PROFITS</ParagraphTitle>
-            <ParagraphText> Collaborate with us to advance your mission for public
-              welfare. Tackle your challenges with fresh and innovative
-              solutions!</ParagraphText>
+            <ParagraphText>
+              {" "}
+              Collaborate with us to advance your mission for public welfare.
+              Tackle your challenges with fresh and innovative solutions!
+            </ParagraphText>
             <button className="text-blueprint-blue border-blueprint-blue flex justify-center items-center w-[200px] p-4 rounded-md border-2 font-poppins font-bold tracking-tight hover:shadow-2xl">
               Collaborate with Us
             </button>
@@ -69,8 +80,10 @@ const HomePage = () => {
 
           <div className="flex flex-col justify-start h-48 gap-2">
             <ParagraphTitle>SPONSORS</ParagraphTitle>
-            <ParagraphText>Support our operation to become part of the changemakers!
-              Elevate your brand while driving meaningful social impact.</ParagraphText>
+            <ParagraphText>
+              Support our operation to become part of the changemakers! Elevate
+              your brand while driving meaningful social impact.
+            </ParagraphText>
             <button className="text-blueprint-blue border-blueprint-blue flex justify-center items-center w-[200px] p-4 rounded-md border-2 font-poppins font-bold tracking-tight hover:shadow-2xl">
               Sponsor Us
             </button>
@@ -81,9 +94,11 @@ const HomePage = () => {
       <div>
         <div style={backgroundStyle}></div>
         <div className="flex flex-col px-3 md:px-[15%] pt-10 gap-4">
-          <PageHeader className="my-5">Our Projects</PageHeader>
-          <SectionHeader>Ongoing</SectionHeader>
-          <div className="flex flex-col lg:grid lg:grid-cols-3 gap-6">
+          <SectionHeader className="md:self-center mb-5">
+            Our Projects
+          </SectionHeader>
+          {/* <SectionHeader>Ongoing</SectionHeader> */}
+          <div className="flex flex-col lg:grid lg:grid-cols-3 gap-9">
             {projects.map((projectKey, index) => (
               <div key={index}>
                 <ProjectCard project={projectKey} />
@@ -96,54 +111,119 @@ const HomePage = () => {
         </div>
       </div>
 
-
-      <div>
+      {/* Desktop Photos */}
+      <div className="hidden md:block px-3 md:px-[15%] mb-16">
         <ParagraphTitle>STAY UPDATED WITH OUR EVENTS</ParagraphTitle>
-        <div className="flex max-md:flex-col justify-between mt-8">
-          <div>
+        <div className="flex max-md:flex-col justify-between mt-8 ">
+          {Event1.map((items, key) => (
+            <div className="relative block group" key={key}>
               <img
-              class="h-48 w-48"
-              src={projects.image}
-              style={{ "min-width": "12rem" }}
-              alt=""
+                class="absolute h-52 inset-0 object-cover group-hover:opacity-50"
+                src={items.img}
+                // style={{ "min-width": "12rem" }}
+                alt=""
               />
-          </div>
-
-          <div>
-              <img
-              class="h-48 w-48"
-              src={projects.image}
-              style={{ "min-width": "12rem" }}
-              alt=""
-              />
-          </div>
-
-          <div>
-              <img
-              class="h-48 w-48"
-              src={projects.image}
-              style={{ "min-width": "12rem" }}
-              alt=""
-              />
-          </div>
-
-          <div>
-              <img
-              class="h-48 w-48"
-              src={projects.image}
-              style={{ "min-width": "12rem" }}
-              alt=""
-              />
-          </div>
-        
+              <div class="relative ">
+                <div class="mt-40">
+                  {/* Hidden content */}
+                  <div
+                    class="transition-all transform 
+                                translate-y-8 opacity-0 
+                                group-hover:opacity-100 
+                                group-hover:translate-y-0"
+                  >
+                    <div class="p-1">
+                      <p class="text-sm text-start text-black">{items.date}</p>
+                    </div>
+                  </div>
+                  {/* End of hidden content */}
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-        <ParagraphText className="mt-5">Follow us on <a href="#" className="underline">Instagram</a></ParagraphText>
-
+        <ParagraphText className="mt-10">
+          Follow us on{" "}
+          <a
+            href="https://www.instagram.com/sfublueprint/"
+            className="underline"
+          >
+            Instagram
+          </a>
+        </ParagraphText>
       </div>
-        
-      
 
-      <div className="px-0.5 mt-9 max-md:max-w-full">
+      {/* Mobile photos */}
+      <div className="block md:hidden flex flex-col align-center content-center justify-center justify-items-center items-center">
+        <div>
+          <ParagraphTitle>STAY UPDATED WITH OUR EVENTS</ParagraphTitle>
+
+          <div className="relative block group">
+            <img
+              class="absolute inset-0 object-cover group-hover:opacity-50"
+              src={"./events/tech.jpg"}
+              // style={{ "min-width": "12rem" }}
+              alt=""
+            />
+            <div class="relative ">
+              <div class="mt-10 mb-40">
+                {/* Hidden content */}
+                <div
+                  class="transition-all transform 
+                                translate-y-8 opacity-0 
+                                group-hover:opacity-100 
+                                group-hover:translate-y-0"
+                >
+                  <div class="p-1">
+                    <p class="text-sm text-start text-black">
+                      November 23rd 2023
+                    </p>
+                  </div>
+                </div>
+                {/* End of hidden content */}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full flex justify-between space-between content-between px-7 my-5">
+          {Event2.map((items, key) => (
+            <div className="relative block group " key={key}>
+              <img
+                class="absolute h-32 w-48 inset-0 object-cover group-hover:opacity-50"
+                src={items.img}
+                alt=""
+              />
+              <div class="relative ">
+                <div class="mt-20">
+                  {/* Hidden content */}
+                  <div
+                    class="transition-all transform 
+                                translate-y-8 opacity-0 
+                                group-hover:opacity-100 
+                                group-hover:translate-y-0"
+                  >
+                    <div class="p-1">
+                      <p class="text-sm text-start text-black">{items.date}</p>
+                    </div>
+                  </div>
+                  {/* End of hidden content */}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <ParagraphText className="mt-5">
+          Follow us on{" "}
+          <a
+            href="https://www.instagram.com/sfublueprint/"
+            className="underline"
+          >
+            Instagram
+          </a>
+        </ParagraphText>
+      </div>
+
+      {/* <div className="px-0.5 mt-9 max-md:max-w-full">
         <div className="flex gap-5 max-md:flex-col max-md:gap-0 max-md:">
           <div className="flex flex-col w-3/12 max-md:ml-0 max-md:w-full">
             <div className="block md:hidden w-screen h-60 bg-zinc-300 justify-self-center rounded-sm "></div>
@@ -158,10 +238,8 @@ const HomePage = () => {
             <div className="block md:hidden w-screen h-60 bg-zinc-300 justify-self-center rounded-sm "></div>
           </div>
         </div>
-      </div>
-
+      </div> */}
     </div>
-
   );
 };
 export default HomePage;
