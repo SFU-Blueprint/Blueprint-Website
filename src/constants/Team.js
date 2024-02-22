db95967cf6b8152499534862c47ca125a0051f2f
Terry <terrykwok147@gmail.com>
2024-02-21 23:23:37 -0800

    Updated the names in Team.js

--- src/constants/Team.js
+++ src/constants/Team.js
@@ -1,158 +1,181 @@
 export const Executives = [
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
+  {
+    title: "Tyler D'silva",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Bhavya Singh",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Rithik Agarwal",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Arthur Zhang",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Avni Kapoor",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Sylvia Zhang",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Long Pham",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Tulika Varma",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Ronney Lok",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Monishka Gautam",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
 ];
 
 export const ProjectLeads = [
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-
+  {
+    title: "Sallin Koutev",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Caitlin Syho",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Colwyn Ralph",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Kian Hoseinkhani",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Karan Pathak",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
 ];
 
 export const Designers = [
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-
+  {
+    title: "Kate Simeonoff",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Eugene Zhu",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Izaz Zubayer",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Manpreet Singh Sarna",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Kaset Le",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Alison Lu",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Jaden L.",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Firm Kong Thong",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Cynthia Cui",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
 ];
 
 export const Developers = [
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
-    {
-        title: "FirstName LastName",
-        img: "/blueprint.jpg",
-        linkedin: "linkedin"
-    },
+  {
+    title: "Duc Dang",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Tomi Lui",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Terry Kwok",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Supriya Dua",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Peiman Zhiani",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Enoch Muwanguzi",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Abishek Deshpande",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Jeffrey Wong",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Brayden Sue",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
+  {
+    title: "Jai Malhi",
+    img: "/blueprint.jpg",
+    linkedin: "linkedin",
+  },
 ];
