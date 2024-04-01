95361082fa1a103b27443c1341af670aebf6f635
Supriya Dua <sda92@sfu.ca>
2024-03-31 19:47:02 -0700

    added 56px padding

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -156,7 +156,7 @@ const AboutPage = () => {
         {/*Start of Team section   */}
         <SectionHeader
           id="Our-team"
-          className="font-anek font-[550] text-4xl flex items-center justify-center md:text-5xl mb-4"
+          className="font-anek font-[550] text-4xl flex items-center justify-center md:text-5xl mb-4 pt-14"
         >
           Our Team
         </SectionHeader>
