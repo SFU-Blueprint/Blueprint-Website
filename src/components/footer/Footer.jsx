b94a6904da360ed739b945c918d8261bdc9ec539
AzkZzz04 <151806867+AzkZzz04@users.noreply.github.com>
2024-04-04 21:00:48 -0700

    fix footer on mobile view

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,31 +13,35 @@ const Footer = () => {
             {/* Render page links */}
             <div className="page-links">
                 {t("pages").map((page) => (
-                    (t(`${page}.anchors`) !== (page + '.anchors')) && <AnchorList page={page} key={page} />
+                    (t(`${page}.anchors`) !== (page + '.anchors')) && <AnchorList page={page} key={page}/>
                 ))}
                 {/* Render connect section */}
                 <div className="connect-container">
                     <div className="text-xl mb-6 font-semibold">{t('connect.name')}</div>
                     <div className="icons-container">
                         {/* Render social media icons */}
-                        {t('connect.links').map((id) => <IconButton id={id} key={id} />)}
+                        {t('connect.links').map((id) => <IconButton id={id} key={id}/>)}
                     </div>
                 </div>
             </div>
             {/* Render horizontal line */}
-            <hr className="my-8" />
+            <hr className="mt-8 mb-4"/>
             {/* Render footer information */}
-            <div className="flex justify-between text-xs items-center">
+            <div className="elements-container">
                 {/* Render logo */}
-                <Logo colour="white" />
+                <Logo colour="white"/>
                 {/* Render common links */}
-                <div>{t('common.blueprint')}</div>
-                <div className="flex gap-4">
+                <div className="icon-name">{t('common.blueprint')}</div>
+                <div className="additional-links">
                     {/* Render additional common links */}
                     <div className="text-hover">{t('common.terms')}</div>
                     <div className="text-hover">{t('common.privacy')}</div>
                     <div className="text-hover">{t('common.cookies')}</div>
                 </div>
+
+            </div>
+            <div className="text-mobile text-end" style={{fontSize: '0.55rem'}}>
+                {t('common.blueprint')}
             </div>
         </footer>
     );
--- src/components/shared/Logo.jsx
+++ src/components/shared/Logo.jsx
@@ -1,15 +1,16 @@
 import {Link} from "react-router-dom";
 import React from "react";
 import { ReactComponent as LogoIcon } from "../../assets/icons/logo.svg";
+import './Logo.css';
 
 const Logo = ({colour = '#0177E8'}) => {
     return (
         <Link
             to="/"
-            className="flex space-x-2 hover:opacity-80 hover:cursor-pointer"
+            className="Logo-Section"
         >
-            <LogoIcon style={{ fill: colour }} />
-            <span style={{ color: colour }} className="font-poppins text-lg font-semibold">
+            <LogoIcon style={{ fill: colour }} className="Logo-Style" />
+            <span style={{ color: colour }} className="Logo-Font">
                 blueprint
             </span>
         </Link>
