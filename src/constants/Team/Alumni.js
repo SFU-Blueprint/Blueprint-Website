af17108979aca9c5ddb6c304d6b303d2fafa8e1b
Sallin142 <Sallin.koutev@gmail.com>
2024-05-01 01:00:32 -0700

    fixed alumni and moved jai to members

--- src/constants/Team/Alumni.js
+++ src/constants/Team/Alumni.js
@@ -2,61 +2,56 @@ export const Alumni = [
   {
     title: "Jaden L.",
     linkedin: "https://www.linkedin.com/in/jaden-s-lee/",
-	position: "Random Person",
+	
   },
   {
     title: "Izaz Zubayer",
     linkedin: "https://www.linkedin.com/in/izazzubayer/",
-	position: "Random Person",
+
   },
   {
     title: "Colwyn Ralph",
     linkedin: "https://www.linkedin.com/in/colwyn-ralph/",
-	position: "Random Person",
+	
   },
   {
     title: "Andy Zhang",
     linkedin: "",
-	position: "Random Person",
-  },
-  {
-    title: "Jai Malhi",
-    linkedin: "https://www.linkedin.com/in/jaimalhi/",
-	position: "Random Person",
+
   },
   {
     title: "Tavleen Sihota",
     linkedin: "https://www.linkedin.com/in/tavleen-sihota/",
-	position: "Random Person",
+
   },
   {
     title: "Yang Liu",
     linkedin: "",
-	position: "Random Person",
+
   },
   {
     title: "Brian Tsou",
     linkedin: "https://www.linkedin.com/in/brianthtsou/",
-	position: "Random Person",
+	
   },
   {
     title: "Mudit Mittal",
     linkedin: "",
-	position: "Random Person",
+	
   },
   {
     title: "Teeya Li",
     linkedin: "",
-	position: "Random Person",
+
   },
   {
     title: "Kumar Shivam",
     linkedin: "https://www.linkedin.com/in/kumarshivam0002/",
-	position: "Random Person",
+
   },
   {
     title: "Manpreet Singh Sarna",
     linkedin: "",
-	position: "Random Person",
+
   },
 ];
