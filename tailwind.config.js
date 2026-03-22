cf37fab36cd7d8e681f3282ca9479e64c1da58d2
Tyler D'Silva <tylerdsilva1@gmail.com>
2026-03-21 18:32:41 -0700

    Added the daisyUI themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,18 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    // Disable dark mode
-    themes: [],
+    themes: [
+      {
+        blueprint: {
+          "primary": "#0177E8",
+          "secondary": "#0D579F",
+          "accent": "#0177E8",
+          "neutral": "#292A3B",
+          "base-100": "#FFFFFF",
+          "base-200": "#F3F3F3",
+          "base-300": "#B8B8B8",
+        },
+      },
+    ],
   },
 };
